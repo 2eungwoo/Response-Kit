f378067ca5f81eda7341ccee82c1579e4ebe22ce
swlee <swo98@naver.com>
2025-10-24 20:04:42 +0900

    init: project init

---
+++ src/test/java/com/github/seungwoo/responsekit/ResponseKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.seungwoo.responsekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
