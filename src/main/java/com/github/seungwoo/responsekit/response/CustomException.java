c41bd0577ac569703748f90f9acd3e2f1d20a0d3
swlee <swo98@naver.com>
2025-10-24 20:18:35 +0900

    add: 공용으로 제공할 상위 exception클래스 생성, 상속 고려해서  접근제어 protected로

---
+++ src/main/java/com/github/seungwoo/responsekit/response/CustomException.java
@@ -0,0 +1,26 @@
+package com.github.seungwoo.responsekit.response;
+
+import lombok.Getter;
+
+/**
+ * 도메인별 CustomException 상속용 추상 클래스.
+ * <p>
+ * 예시:
+ * <pre>
+ * public class UserNotFoundException extends CustomException {
+ *     public UserNotFoundException() {
+ *         super(CommonResponseCode.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+ *     }
+ * }
+ * </pre>
+ */
+@Getter
+public abstract class CustomException extends RuntimeException {
+
+    private final BaseResponseCode responseCode;
+
+    protected CustomException(BaseResponseCode responseCode, String message) {
+        super(message);
+        this.responseCode = responseCode;
+    }
+}
